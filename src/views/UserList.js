8a637ee504f10ebe352a7b33879e8182cb2243c4
TiagoM13 <thyagomotha2000@gmail.com>
2023-09-03 15:47:22 -0300

    feat: add actions functions and deletion alert

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,11 +1,41 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { FlatList, View } from 'react-native';
-import { Avatar, ListItem } from 'react-native-elements';
+import { FlatList, View, Alert } from 'react-native';
+import { Avatar, Button, ListItem, Icon } from 'react-native-elements';
 
 import users from '../data/users';
 
 export default props => {
+
+  function confirmuserDeletion(user) {
+    Alert.alert('Excluir usuário', 'Deseja excluir o usuário', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('Delete ' + user.id)
+        }
+      }, {
+        text: 'Não'
+      }
+    ])
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type='clear'
+          icon={<Icon name='edit' size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmuserDeletion(user)}
+          type='clear'
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    )
+  }
 
   function getUserItem({ item: user }) {
     return (
@@ -19,6 +49,7 @@ export default props => {
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+        {getActions(user)}
       </ListItem>
     )
   }
