c501d9b45b0ad1208a719766cd40b13f54478d19
TiagoM13 <thyagomotha2000@gmail.com>
2023-09-03 15:16:46 -0300

    feat: add user listing

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,7 +1,35 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+
+import users from '../data/users';
 
 export default props => {
-  return <Text>UserList</Text>;
+
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem bottomDivider>
+        <Avatar
+          size={40}
+          rounded
+          source={{ uri: user.avatarUrl }}
+        />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    )
+  }
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  )
 };
