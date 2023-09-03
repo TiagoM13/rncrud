6c57ffc6d27944fc9b10cf7f4c5f6f79900591f9
TiagoM13 <thyagomotha2000@gmail.com>
2023-09-03 16:27:06 -0300

    feat: add form screen

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,7 +1,62 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
 
-export default props => {
-  return <Text>UserForm</Text>;
+export default ({ route, navigation }) => {
+  const [user, setUser] = React.useState(route.params ? route.params : {});
+
+  console.log({ user })
+
+  return (
+    <View style={style.form}>
+      <Text style={style.label}>Nome</Text>
+      <TextInput
+        onChangeText={name => setUser({ ...user, name })}
+        placeholder="Informe o Nome"
+        value={user.name}
+        style={style.input}
+      />
+
+      <Text style={style.label}>E-mail</Text>
+      <TextInput
+        onChangeText={email => setUser({ ...user, email })}
+        placeholder="Informe o E-mail"
+        value={user.email}
+        style={style.input}
+      />
+
+      <Text style={style.label}>URL do Avatar</Text>
+      <TextInput
+        onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
+        placeholder="Informe a URl do Avatar"
+        value={user.avatarUrl}
+        style={style.input}
+      />
+
+      <Button
+        title='Salvar'
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  )
 };
+
+const style = StyleSheet.create({
+  label: {
+    color: '#000',
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'gray',
+    marginBottom: 16,
+    fontSize: 16,
+    color: '#000'
+  },
+})
